75d4705699a811f11c3ec9c061d86c9bd0207fb9
qianlanyuyun <2502203399@qq.com>
2023-05-22 16:24:59 +0800

    喵喵喵

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,2 @@
 export declare const Greeter: (name: string) => string;
+export declare const am: (name: object) => string;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export const Greeter = (name: string) => `Hello ${name}`;
+export const am = (name: object) => `${JSON.stringify(name)}`;
